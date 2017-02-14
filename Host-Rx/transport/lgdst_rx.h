b99dbe4c6be0469bd46ec66ca245f39b3cd68961
liyen ho <lho@yuneecusa.com>
2017-02-14 13:45:21 -0800

    major update on everything for this repo

---
+++ Host-Rx/transport/lgdst_rx.h
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stdbool.h>
+void sigint_handler_rx(int signum);
+#if defined(PES_HDR_PROT) ||defined(PES_FRM_PROT) ||defined(PES_FRM_PROT1)
+  void lgdst_ts_rx(uint8_t *tsbuf0, uint32_t *size);
+#else
+  void lgdst_ts_rx(uint8_t *tsbuf0);
+#endif
+void lgdst_ctl_rec_rx(unsigned char *rpacket);
+void lgdst_ctl_snd_rx(unsigned char *tpacket);
+#if (/*1*/0)
+  int lgdst_init_rx();
+#else
+  int lgdst_init_rx(int argc, char **argv);
+#endif
+void lgdst_deinit_rx(int rcode);
+/* internal function, do not invoke directly but using command line */
+bool lgdst_upgrade_rx(int argc, char **argv);  // return -1 when failed, liyenho
+
